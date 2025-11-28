7baaf84b470511c125700dea8aed45eab0912495
Luca Ottaviano <lucatremila@gmail.com>
2025-11-28 19:10:26 +0100

    fix hidden files

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,8 @@ export const buildTree = (paths: string[]): TreeItem[] => {
         if (!node) {
             let isFile = false
             if (head.includes(".")) {
-                isFile = head[0] === '.'
+                console.log(head[0] + ' includes dot')
+                isFile = head[0] !== '.'
             }
             node = {
                 name: head,
@@ -42,5 +43,6 @@ export const buildTree = (paths: string[]): TreeItem[] => {
         insertPath(tree, path);
     }
 
+    console.log('///////')
     return tree;
 };
